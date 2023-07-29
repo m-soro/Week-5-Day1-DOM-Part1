78031b9b0407293cf9174ecddea37ba716472316
Mark Soro <napoleon_soro@rocketmail.com>
2023-07-28 20:25:48 -0400

    completed v1 of assignment

---
+++ README.md
@@ -0,0 +1,3 @@
+# DOM "Menu" Lab Part-1
+
+[REPL Link](https://replit.com/@msoro/DOM-Menu-LabWk5-Day2)
---
+++ script.js
@@ -0,0 +1,24 @@
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+menuLinks.forEach((element) => {
+  const linkTag = document.createElement("a");
+  linkTag.innerText = element.text;
+  linkTag.setAttribute("href", element.href);
+  topMenuEl.appendChild(linkTag);
+});
